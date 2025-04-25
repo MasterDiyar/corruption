caa9f039ae428e580fb5e6aade8fc4545d4099f6
Diyarka <sikizhanov92@BK.RU>
2025-04-25 14:56:38 +0500

    test1

--- player/Zhukov.cs
+++ player/Zhukov.cs
@@ -3,4 +3,21 @@ using System;
 
 public partial class Zhukov : Node2D
 {
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Process(double delta)
+    {
+        
+    }
+
+    public void walk(){
+
+    }
+
+    public void attack(){
+        
+    }
 }
