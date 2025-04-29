89c1e1baf818ef1945ca2457191aba26b857b5d1
Diyarka <sikizhanov92@BK.RU>
2025-04-29 12:39:57 +0500

    item and inventory

---
+++ Main.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+    public int[] inventory = {1, 2, 3};
+    public int[,] upgrades = {{0,0},{0,0},{0,0}};
+    public float modifier = 1;
+    
+
+}
--- player/Zhukov.cs
+++ player/Zhukov.cs
@@ -6,8 +6,14 @@ public partial class Zhukov : CharacterBody2D
     private Sprite2D arch;
     public float hp = 10;
     private AnimatedSprite2D icon;
+    Node attac, dash;
+    
+    Area2D taker;
     public override void _Ready()
     {
+        attac = GetNode("attack");
+        taker = GetNode<Area2D>("takeitem");
+        taker.AreaEntered += lol;
         icon = (AnimatedSprite2D)GetNode("Icon");
         icon.Play("idle");
         arch = GetNode<Sprite2D>("Arch");
@@ -29,6 +35,18 @@ public partial class Zhukov : CharacterBody2D
         var angle = GetAngleTo(GetGlobalMousePosition());
         arch.Position = 80 * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
         arch.Rotation = angle + Mathf.Pi / 2;
+    }
+
+    void lol (Area2D a){
+        if (a.HasMeta("type")) switch ((string)a.GetMeta("type")){
+            case "patron":
+                if (attac is Attack arc){
+                    arc.totalAmmo += 4;
+                }
+                GD.Print("taker");
+                a.QueueFree();
+            break;
+        }
     }
     
 }
